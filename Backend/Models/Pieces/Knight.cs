480446edffd7af3f09caf351e56618c9e81c054f
ProgrammistIT <lamthaiqwer@gmail.com>
2026-05-26 17:57:40 +0300

    Add GetValidMoves for Knight

--- Backend/Models/Pieces/Knight.cs
+++ Backend/Models/Pieces/Knight.cs
@@ -5,4 +5,10 @@ namespace Chess.Backend.Models.Pieces;
 public class Knight : Piece
 {
     public Knight(PieceColor color) : base(color, PieceType.Knight) {}
+
+    public override IEnumerable<(int Row, int Col)> GetValidMoves(Square[,] square, int row, int column)
+    {
+        (int, int)[] directions = [(1, 2), (1, -2), (-1, 2), (-1, -2), (2, 1), (-2, 1), (2, -1), (-2, -1)];
+        return Jump(square, row, column, directions);
+    }
 }
