9a4cc749d8cecb2e473ef9351d80ec42cb3fb987
ProgrammistIT <lamthaiqwer@gmail.com>
2026-05-27 10:54:58 +0300

    Optimizing code

--- Backend/Models/Pieces/Bishop.cs
+++ Backend/Models/Pieces/Bishop.cs
@@ -2,7 +2,7 @@ using Chess.Backend.Enums;
 
 namespace Chess.Backend.Models.Pieces;
 
-public class Bishop : Rook
+public class Bishop : Piece
 {
     public Bishop(PieceColor color) : base(color,  PieceType.Bishop) {}
 
