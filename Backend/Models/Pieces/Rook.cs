072a6ad7a004ed825e380ca747558e86915ee15c
ProgrammistIT <166043743+ProgrammistIT@users.noreply.github.com>
2026-05-29 23:03:44 +0300

    Update Rook.cs

--- Backend/Models/Pieces/Rook.cs
+++ Backend/Models/Pieces/Rook.cs
@@ -5,33 +5,12 @@ namespace Chess.Backend.Models.Pieces;
 public class Rook : Piece
 {
     public Rook(PieceColor color) : base(color, PieceType.Rook) {}
-    protected Rook(PieceColor color, PieceType type) : base(color, type) {}
     public override IEnumerable<(int Row, int Col)> GetValidMoves(Square[,] square, int row, int column)
     {
         foreach (var (dr, dc) in new[] {(-1, 0), (1, 0), (0, -1), (0, 1) })
         {
             foreach (var move in Slide(square, row, column, dr, dc))
                 yield return move;
-        }
-    }
-
-    protected IEnumerable<(int, int)> Slide(Square[,] square, int row, int column, int dr, int dc)
-    {
-        int r = row + dr;
-        int c = column + dc;
-
-        while (r is >= 0 and < 8 && c is >= 0 and < 8)
-        {
-            if (square[r, c].Piece == null)
-                yield return (r, c);
-            else
-            {
-                if (square[r, c].Piece!.Color != Color)
-                    yield return (r, c);
-                break;
-            }
-            r += dr;
-            c += dc;
         }
     }
 }
